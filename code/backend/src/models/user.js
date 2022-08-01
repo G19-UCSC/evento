a3a285b8194e849233caee90b8bab9bfbeacad59
mtbstn <thushibstn24@gmail.com>
2022-08-01 23:34:31 +0530

    add userSchema and user model

---
+++ code/backend/src/models/user.js
@@ -0,0 +1,32 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('../utilities/dbHelper');
+
+const User = db.define(
+  'user',
+  {
+    _userid: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    firstname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    lastname: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    }
+  }
+  ,{
+    logging: false,
+  }
+);
+
+Event.sync()
+
+module.exports= User;
