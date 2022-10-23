311d6bfae75e1b72d012f3d305f3ede26596d4dd
VinoVK08 <vinothinivijayakumaran08@gmail.com>
2022-10-23 13:43:07 +0530

    modified product payment model

--- code/backend/src/models/productPayment.js
+++ code/backend/src/models/productPayment.js
@@ -5,26 +5,59 @@ const ProductPayment = db.define(
   'productPayment',
   {
     
+    _id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    userid: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
     productid: {
       type: DataTypes.STRING(30),
       allowNull: false,
     },
-    
-    paymentid: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-    },
-    count: {
+    quantity: {
       type: DataTypes.INTEGER(30),
       allowNull: false,
     }, 
+    purchaseDate: {
+      type: DataTypes.DATE(),
+      allowNull: true,
+    },
+    price: {
+      type: DataTypes.FLOAT(3),
+      allowNull: true,
+    }, 
+    commission: {
+      type: DataTypes.FLOAT(3),
+      allowNull: true,
+    }, 
+    CusPayStatus: {
+      type: DataTypes.ENUM('Pending','Received'),
+      allowNull: false,
+    }, 
+    CusPayDate: {
+      type: DataTypes.DATE(),
+      allowNull: true,
+    },
+    ProviderPayStatus: {
+      type: DataTypes.ENUM('Pending', 'Paid'),
+      allowNull: false,
+    },
+    ProviderPayDate: {
+      type: DataTypes.DATE(),
+      allowNull: true,
+    }, 
+
   }
   ,{
     logging: false,
   }
 );
 
-ProductPayment.sync({force:true})
+ProductPayment.sync()
 
 // Product.sync().then((res) => {
 //   Product.create({ 
