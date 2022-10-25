eafd8087864962a3924e48971e27bab23609b3f4
mtbstn <thushibstn24@gmail.com>
2022-10-25 08:51:35 +0530

    add serviceBooking routes

--- code/backend/src/routes/productPaymentRoutes.js
+++ code/backend/src/routes/productPaymentRoutes.js
@@ -2,11 +2,11 @@ const express = require('express')
 const productPaymentController = require('../controllers/productPaymentController')
 const router = express.Router()
 
-// router.route('/').get(productPaymentController.getAllProductPayments)
+router.route('/').get(productPaymentController.getAllProductPayments)
 router.route('/').post(productPaymentController.setProductPayment)
-// router.route('/:id').get(productPaymentController.getProductPayment)
-// router.route('/:id').put(productPaymentController.updateProductPayment)
-// router.route('/:id').delete(productPaymentController.deleteProductPayment)
+router.route('/:id').get(productPaymentController.getProductPayment)
+router.route('/:id').put(productPaymentController.updateProductPayment)
+router.route('/:id').delete(productPaymentController.deleteProductPayment)
 
 
 module.exports = router 
